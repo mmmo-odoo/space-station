6f5f77dac9ed718d38aecf7841bbc81c7976a810
mmmo-odoo <mmmo@odoo.com>
2024-02-09 15:58:22 +0530

    module is shown in app's but unable to activate

---
+++ station/__manifest__.py
@@ -0,0 +1,18 @@
+# The __manifest__.py file must describe our module and cannot remain empty. Its only required field is the name, but it usually contains much more information.
+{
+    'name' : 'Space Station',
+    'version' : '1.0',
+    'author': "Mayank Mangal Mourya",
+    'summary' : 'Track Real Time Requirements Deal and Opportunities',
+    'description' : """
+        Description text
+    """,
+    'depend' : [
+        'base'
+    ],
+    'data' : [],
+    'test' : [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
---
+++ station/models/station_property.py
@@ -0,0 +1,6 @@
+from odoo import models, fields
+
+class StationProperty(models.Model) :
+    _name = 'station.property'
+    _description = "Real time space station manager"
+    
